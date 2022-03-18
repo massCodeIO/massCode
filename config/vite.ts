828375b5135abbd061bc4f68d29cc58960f797a1
Anton Reshetov <reshetov.art@gmail.com>
2022-03-18 18:28:56 +0300

    build: add auto imports

--- config/vite.ts
+++ config/vite.ts
@@ -1,24 +1,36 @@
 import path from 'path'
 import vuePlugin from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
-/**
- * https://vitejs.dev/config
- */
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+
+const pathSrc = path.resolve(__dirname, '../../src/renderer')
+const pathOut = path.resolve(__dirname, '../renderer')
+
 export default defineConfig({
-  root: path.resolve(__dirname, '../../src/renderer'),
+  root: pathSrc,
   publicDir: 'public',
   server: {
     port: 8080,
     open: false
   },
   build: {
-    outDir: path.resolve(__dirname, '../renderer'),
+    outDir: pathOut,
     emptyOutDir: true
   },
-  plugins: [vuePlugin()],
+  plugins: [
+    vuePlugin(),
+    AutoImport({
+      dts: `${pathSrc}/types/auto-imports.d.ts`
+    }),
+    Components({
+      dts: `${pathSrc}/types/components.d.ts`,
+      dirs: [`${pathSrc}/components`]
+    })
+  ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, '../../src/renderer')
+      '@': pathSrc
     }
   }
 })

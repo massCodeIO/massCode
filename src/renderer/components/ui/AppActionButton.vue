20e739e358c160c8934010c4ae3a8ebcf00efd20
Anton Reshetov <reshetov.art@gmail.com>
2022-03-25 09:11:02 +0300

    feat(ui): add `AppActionButton`

---
+++ src/renderer/components/ui/AppActionButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="button">
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+.button {
+  display: flex;
+  align-items: center;
+  padding: 4px 6px;
+  border-radius: 4px;
+  svg {
+    fill: var(--color-contrast-medium);
+  }
+  &:hover {
+    background-color: var(--color-contrast-lower);
+  }
+}
+</style>

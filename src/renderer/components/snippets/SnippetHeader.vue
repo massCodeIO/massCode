2798fa0b0ad6c1c84ea214355162175254b1cf91
Anton Reshetov <reshetov.art@gmail.com>
2022-03-29 10:34:08 +0300

    feat(snippets): add ediable snippet name, focus name for new snippet

--- src/renderer/components/snippets/SnippetHeader.vue
+++ src/renderer/components/snippets/SnippetHeader.vue
@@ -2,7 +2,12 @@
   <div class="header">
     <div class="top">
       <div class="name">
-        {{ snippetStore.snippet?.name }}
+        <input
+          ref="inputRef"
+          v-model="name"
+          type="text"
+          placeholder="Type snippet name"
+        >
       </div>
       <div class="action">
         <AppActionButton>
@@ -20,9 +25,26 @@
 </template>
 
 <script setup lang="ts">
+import { emitter } from '@/composable'
 import { useSnippetStore } from '@/store/snippets'
+import { useDebounceFn } from '@vueuse/core'
+import { computed, ref } from 'vue'
 
 const snippetStore = useSnippetStore()
+const inputRef = ref<HTMLInputElement>()
+
+const name = computed({
+  get: () => snippetStore.snippet?.name,
+  set: useDebounceFn(
+    v =>
+      snippetStore.patchSnippetsById(snippetStore.selectedId!, { name: v }),
+    300
+  )
+})
+
+emitter.on('focus:snippet-name', () => {
+  inputRef.value?.select()
+})
 </script>
 
 <style lang="scss" scoped>
@@ -36,6 +58,13 @@ const snippetStore = useSnippetStore()
   }
   .name {
     font-size: 16px;
+    width: 100%;
+    input {
+      border: 0;
+      width: 100%;
+      outline: none;
+      text-overflow: ellipsis;
+    }
   }
   .action {
     display: flex;

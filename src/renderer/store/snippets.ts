6fdf62095083d6597a77a11629f0348a70c2ca4a
Anton Reshetov <reshetov.art@gmail.com>
2022-03-29 15:28:29 +0300

    feat(snippets): add fragment coun

--- src/renderer/store/snippets.ts
+++ src/renderer/store/snippets.ts
@@ -19,6 +19,7 @@ export const useSnippetStore = defineStore('snippets', {
     currentContent: state => state.snippet?.content[state.fragment].value,
     currentLanguage: state => state.snippet?.content[state.fragment].language,
     fragmentLabels: state => state.snippet?.content.map(i => i.label),
+    fragmentCount: state => state.snippet?.content.length,
     isFragmentsShow () {
       return this.fragmentLabels?.length > 1
     }

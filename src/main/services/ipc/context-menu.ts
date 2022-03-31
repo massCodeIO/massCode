07cfb7432ad67992e13a57cf42c243a923879b5f
Anton Reshetov <reshetov.art@gmail.com>
2022-03-31 09:42:53 +0300

    fix(main: ipc): add typing

--- src/main/services/ipc/context-menu.ts
+++ src/main/services/ipc/context-menu.ts
@@ -1,40 +1,44 @@
 import { createPopupMenu } from '../../components/menu'
 import { dialog, ipcMain } from 'electron'
+import type { ContextMenuPayload, ContextMenuResponse } from '../../types'
 
 export const subscribeToContextMenu = () => {
-  ipcMain.handle('context-menu:snippet-fragment', async (event, payload) => {
-    const { name } = payload
+  ipcMain.handle<ContextMenuPayload, ContextMenuResponse>(
+    'context-menu:snippet-fragment',
+    async (event, payload) => {
+      const { name } = payload
 
-    return new Promise(resolve => {
-      createPopupMenu([
-        {
-          label: `Rename "${name}"`,
-          click: () =>
-            resolve({
-              action: 'rename',
-              payload: payload
-            })
-        },
-        { type: 'separator' },
-        {
-          label: `Delete "${name}"`,
-          click: () => {
-            const buttonId = dialog.showMessageBoxSync({
-              message: `Are you sure you want to permanently delete "${name}"?`,
-              detail: 'You cannot undo this action.',
-              buttons: ['Delete', 'Cancel'],
-              defaultId: 0,
-              cancelId: 1
-            })
-            if (buttonId === 0) {
+      return new Promise(resolve => {
+        createPopupMenu([
+          {
+            label: `Rename "${name}"`,
+            click: () =>
               resolve({
-                action: 'delete',
-                payload: payload
+                action: 'rename',
+                data: payload
               })
+          },
+          { type: 'separator' },
+          {
+            label: `Delete "${name}"`,
+            click: () => {
+              const buttonId = dialog.showMessageBoxSync({
+                message: `Are you sure you want to permanently delete "${name}"?`,
+                detail: 'You cannot undo this action.',
+                buttons: ['Delete', 'Cancel'],
+                defaultId: 0,
+                cancelId: 1
+              })
+              if (buttonId === 0) {
+                resolve({
+                  action: 'delete',
+                  data: payload
+                })
+              }
             }
           }
-        }
-      ])
-    })
-  })
+        ])
+      })
+    }
+  )
 }

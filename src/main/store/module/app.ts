075b4907de48d5c97ff1ee2a9ba5414ebf60afb4
Anton Reshetov <reshetov.art@gmail.com>
2022-03-31 11:27:14 +0300

    fix(main: store): set cwd to v2 to prevent conflict

--- src/main/store/module/app.ts
+++ src/main/store/module/app.ts
@@ -1,9 +1,10 @@
 import Store from 'electron-store'
+import { STORE_NAME } from '..'
 import type { AppStore } from './types'
 
 export default new Store<AppStore>({
   name: 'app',
-  cwd: 'massCode',
+  cwd: 'v2',
 
   defaults: {
     bounds: {}
--- src/main/store/module/preferences.ts
+++ src/main/store/module/preferences.ts
@@ -1,5 +1,6 @@
 import Store from 'electron-store'
 import { homedir, platform } from 'os'
+import { STORE_NAME } from '..'
 import type { PreferencesStore } from './types'
 
 const isDev = process.env.NODE_ENV === 'development'
@@ -15,7 +16,7 @@ const backupPath = isWin ? `${defaultPath}\\backups` : `${defaultPath}/backups`
 
 export default new Store<PreferencesStore>({
   name: 'preferences',
-  cwd: 'massCode',
+  cwd: 'v2',
 
   defaults: {
     storagePath: defaultPath,

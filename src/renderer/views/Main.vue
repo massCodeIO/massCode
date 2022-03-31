cb3507db680dfc0df3b02afd11cd02dac083a5fe
Anton Reshetov <reshetov.art@gmail.com>
2022-03-31 10:27:56 +0300

    feat: update init

--- src/renderer/views/Main.vue
+++ src/renderer/views/Main.vue
@@ -16,13 +16,14 @@ const snippetStore = useSnippetStore()
 
 const init = async () => {
   const storedFolderId = store.app.get('selectedFolderId')
+  const storedFolderIds = store.app.get('selectedFolderIds')
   const storedSnippetId = store.app.get('selectedSnippetId')
   const firstFolderId = folderStore.main[0]?.id
   const firstSnippetId = snippetStore.snippets[0]?.id
 
   await folderStore.getFolders()
   folderStore.selectId(storedFolderId || firstFolderId)
-  await snippetStore.getSnippetsByFolderId(storedFolderId || firstFolderId)
+  await snippetStore.getSnippetsByFolderIds(storedFolderIds || [firstFolderId])
   await snippetStore.getSnippetsById(storedSnippetId || firstSnippetId)
 }
 

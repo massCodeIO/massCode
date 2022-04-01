9558d4a65ff6b56a0193afd178ce472fb1d44c42
Anton Reshetov <reshetov.art@gmail.com>
2022-04-01 11:34:50 +0300

    types: add more types

--- src/renderer/components/sidebar/types/index.d.ts
+++ src/renderer/components/sidebar/types/index.d.ts
@@ -1,6 +1,15 @@
+import type { Folder } from '@@/types/db'
+import type { FunctionalComponent } from 'vue'
+
 export type Tab = 'library' | 'tags'
+export type SystemFolderAlias = 'inbox' | 'favorites' | 'trash' | 'all'
 
 export interface Tabs {
   label: string
   value: Tab
+}
+
+export interface SidebarSystemFolder extends Partial<Folder> {
+  icon: FunctionalComponent
+  alias: SystemFolderAlias
 }

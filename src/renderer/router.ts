7762315e512c3135c8d0f08b670c110c2f251c74
Anton Reshetov <reshetov.art@gmail.com>
2022-04-09 10:50:05 +0300

    fix(router): import all views at once

--- src/renderer/router.ts
+++ src/renderer/router.ts
@@ -1,14 +1,12 @@
 import type { RouteRecordRaw } from 'vue-router'
 import { createWebHistory, createRouter } from 'vue-router'
 import Main from './views/Main.vue'
+import Preferences from './views/Preferences.vue'
 
 const history = createWebHistory()
 const routes: RouteRecordRaw[] = [
   { path: '/', component: Main },
-  {
-    path: '/preferences',
-    component: import('./views/Preferences.vue')
-  }
+  { path: '/preferences', component: Preferences }
 ]
 
 const router = createRouter({ history, routes })

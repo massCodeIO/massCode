1733ab48dc575d40994a2e935b11c4b8f1912002
sangsq <sang93@qq.com>
2022-07-24 00:17:19 +0800

    chore: add save screenshot button tooltip

--- src/renderer/components/screenshot/TheScreenshot.vue
+++ src/renderer/components/screenshot/TheScreenshot.vue
@@ -24,7 +24,9 @@
         </div>
       </div>
       <div class="right">
-        <AppActionButton @click="onSaveScreenshot">
+        <AppActionButton
+          v-tooltip="i18n.t('saveScreenshot')"
+          @click="onSaveScreenshot">
           <UniconsFileDownload />
         </AppActionButton>
       </div>

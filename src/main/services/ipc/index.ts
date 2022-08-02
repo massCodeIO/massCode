a38c1662913977ae1b85885b02f1f32364db170b
Anton Reshetov <reshetov.art@gmail.com>
2022-08-02 14:10:42 +0300

    feat(main: ipc): add read `static` folder

--- src/main/services/ipc/index.ts
+++ src/main/services/ipc/index.ts
@@ -1,9 +1,11 @@
 import { subscribeToContextMenu } from './context-menu'
 import { subscribeToNotification } from './notifications'
 import { subscribeToPrettier } from './prettier'
+import { subscribeToFs } from './fs'
 
 export const subscribeToChannels = () => {
   subscribeToContextMenu()
   subscribeToNotification()
   subscribeToPrettier()
+  subscribeToFs()
 }

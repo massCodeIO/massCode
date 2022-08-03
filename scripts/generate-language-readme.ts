565c9661ec3d83bcb9dfa304b2807b5fdf9df46d
Anton Reshetov <reshetov.art@gmail.com>
2022-08-03 16:20:34 +0300

    chore(scripts): add language readme generator

---
+++ scripts/generate-language-readme.ts
@@ -0,0 +1,28 @@
+import fs from 'fs-extra'
+import { join } from 'path'
+import Handlebars from 'handlebars'
+import { languages } from '../src/renderer/components/editor/languages'
+
+const sorted = languages.sort((a, b) =>
+  a.name.toLowerCase() > b.name.toLowerCase() ? 1 : -1
+)
+console.log(sorted)
+
+const source = `# Languages list
+
+This is a list of language that supports in massCode.
+
+{{#each sorted}}
+- {{this.name}}
+{{/each}}
+`
+
+const template = Handlebars.compile(source)
+const result = template({ sorted })
+
+fs.writeFile(
+  join(__dirname, '../../src/renderer/components/editor/README.md'),
+  result,
+  { encoding: 'utf-8' }
+)
+console.log('Done')

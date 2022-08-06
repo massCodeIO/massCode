c729a3ea09ccb6e25119ca9e6ffe83af3a8bf3f4
Anton Reshetov <reshetov.art@gmail.com>
2022-08-06 10:49:31 +0300

    refactor: load onigasm (#193)

--- src/main/services/ipc/index.ts
+++ src/main/services/ipc/index.ts
@@ -1,11 +1,9 @@
 import { subscribeToContextMenu } from './context-menu'
 import { subscribeToNotification } from './notifications'
 import { subscribeToPrettier } from './prettier'
-import { subscribeToFs } from './fs'
 
 export const subscribeToChannels = () => {
   subscribeToContextMenu()
   subscribeToNotification()
   subscribeToPrettier()
-  subscribeToFs()
 }

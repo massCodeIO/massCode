bad01cefabadb31a51705c2aaae623fee8c6e841
Anton Reshetov <reshetov.art@gmail.com>
2023-01-07 12:47:55 +0300

    chore(i18n): sort languages

--- src/main/services/i18n/language.ts
+++ src/main/services/i18n/language.ts
@@ -3,11 +3,11 @@ export const language = {
   el: 'Ελληνικά',
   en: 'English',
   es_ES: 'Español',
+  ja_JP: '日本語',
   pt_BR: 'Português (Brasil)',
-  uk_UA: 'Українська',
   ru: 'Русский',
+  uk_UA: 'Українська',
   zh_CN: '中文 (简体)',
   zh_TW: '中文 (繁體)',
-  zh_HK: '中文 (繁體 香港特別行政區)',
-  ja_JP: '日本語'
+  zh_HK: '中文 (繁體 香港特別行政區)'
 }

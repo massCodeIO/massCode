9f55ff1ec219c81281c30e6733407230d6a30b44
Szabolcs Zsoldos <zsoldos@materom.ro>
2023-01-24 10:40:34 +0200

    feat(i18n): add Romanian locales (#334)

--- src/main/services/i18n/language.ts
+++ src/main/services/i18n/language.ts
@@ -6,9 +6,10 @@ export const language = {
   es_ES: 'Español',
   ja_JP: '日本語',
   pt_BR: 'Português (Brasil)',
+  ro: 'Română',
   ru: 'Русский',
   uk_UA: 'Українська',
   zh_CN: '中文 (简体)',
-  zh_TW: '中文 (繁體)',
-  zh_HK: '中文 (繁體 香港特別行政區)'
+  zh_HK: '中文 (繁體 香港特別行政區)',
+  zh_TW: '中文 (繁體)'
 }

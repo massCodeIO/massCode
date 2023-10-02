bc6da09021d95eeb9ba35f2a5373e464b155df88
Emircan ERKUL <develop@emircanerkul.com>
2023-10-02 08:52:21 +0300

    feat(i18n): add Turkish locales (#412)

--- src/main/services/i18n/language.ts
+++ src/main/services/i18n/language.ts
@@ -8,6 +8,7 @@ export const language = {
   pt_BR: 'Português (Brasil)',
   ro_RO: 'Română',
   ru: 'Русский',
+  tr: 'Türkçe',
   uk_UA: 'Українська',
   zh_CN: '中文 (简体)',
   zh_HK: '中文 (繁體 香港特別行政區)',

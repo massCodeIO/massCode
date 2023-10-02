2fc4e34c235cd9ca1826c8c0b51d6a2502cde0fa
Anton Reshetov <reshetov.art@gmail.com>
2023-10-02 08:49:20 +0300

    chore: sort

--- src/main/services/i18n/language.ts
+++ src/main/services/i18n/language.ts
@@ -8,8 +8,8 @@ export const language = {
   pt_BR: 'Português (Brasil)',
   ro_RO: 'Română',
   ru: 'Русский',
-  uk_UA: 'Українська',
   tr: 'Türkçe',
+  uk_UA: 'Українська',
   zh_CN: '中文 (简体)',
   zh_HK: '中文 (繁體 香港特別行政區)',
   zh_TW: '中文 (繁體)'

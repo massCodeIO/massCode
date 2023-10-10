5b27fdad3180a43177a2b047afe221114efe39f2
fred <domaine75@gmail.com>
2023-10-10 05:28:55 +0100

    feat(i18n): add French locales (#417)

--- src/main/services/i18n/language.ts
+++ src/main/services/i18n/language.ts
@@ -4,6 +4,7 @@ export const language = {
   el: 'Ελληνικά',
   en: 'English',
   es_ES: 'Español',
+  fr_FR: 'French',
   ja_JP: '日本語',
   pt_BR: 'Português (Brasil)',
   ro_RO: 'Română',
@@ -12,5 +13,5 @@ export const language = {
   uk_UA: 'Українська',
   zh_CN: '中文 (简体)',
   zh_HK: '中文 (繁體 香港特別行政區)',
-  zh_TW: '中文 (繁體)'
+  zh_TW: '中文 (繁體)',
 }

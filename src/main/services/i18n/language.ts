cd44fb4d87ff873d6483c1eaeff3f45f5a9a287f
Jarosław Pietras <jarko2227@gmail.com>
2024-01-20 14:27:21 +0100

    feat(i18n): add Polish locales

--- src/main/services/i18n/language.ts
+++ src/main/services/i18n/language.ts
@@ -7,6 +7,7 @@ export const language = {
   fa_IR: 'فارسی',
   fr_FR: 'French',
   ja_JP: '日本語',
+  pl: 'Polski',
   pt_BR: 'Português (Brasil)',
   ro_RO: 'Română',
   ru: 'Русский',
@@ -14,5 +15,5 @@ export const language = {
   uk_UA: 'Українська',
   zh_CN: '中文 (简体)',
   zh_HK: '中文 (繁體 香港特別行政區)',
-  zh_TW: '中文 (繁體)',
+  zh_TW: '中文 (繁體)'
 }

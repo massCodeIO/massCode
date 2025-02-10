f220357dfa880c2fb0fddc4f43f353282b073c8b
Anton Reshetov <reshetov.art@gmail.com>
2025-02-10 08:52:50 +0300

    feat: add window menu with minimize option

--- src/main/menu/main.ts
+++ src/main/menu/main.ts
@@ -519,6 +519,14 @@ const historyMenu: MenuItemConstructorOptions[] = [
   }
 ]
 
+const windowMenu: MenuItemConstructorOptions[] = [
+  {
+    label: i18n.t('menu:window.minimize'),
+    accelerator: 'CommandOrControl+M',
+    role: 'minimize'
+  }
+]
+
 const menuItems: MenuItemConstructorOptions[] = [
   {
     label: i18n.t('menu:app.label'),
@@ -547,6 +555,10 @@ const menuItems: MenuItemConstructorOptions[] = [
   {
     label: i18n.t('menu:history.label'),
     submenu: historyMenu
+  },
+  {
+    label: i18n.t('menu:window.label'),
+    submenu: windowMenu
   },
   {
     label: i18n.t('menu:help.label'),

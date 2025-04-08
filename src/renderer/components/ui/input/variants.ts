5bb2fe42aab8c53ce376f96ff34e7bd39b44b9db
Anton Reshetov <reshetov.art@gmail.com>
2025-04-08 09:32:25 +0300

    fix(input): update model type to accept string or number

--- src/renderer/components/ui/input/variants.ts
+++ src/renderer/components/ui/input/variants.ts
@@ -2,7 +2,7 @@ import type { VariantProps } from 'class-variance-authority'
 import { cva } from 'class-variance-authority'
 
 export const variants = cva(
-  'w-full rounded-md focus:outline-none placeholder:text-text-muted py-0.5 px-2 border dark:bg-black/10 bg-black/3',
+  'w-full rounded-md focus:outline-none placeholder:text-text-muted py-0.5 px-2 border dark:bg-black/10 bg-black/3 [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none',
   {
     variants: {
       variant: {

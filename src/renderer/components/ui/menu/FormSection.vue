2cd847d0e0a0c5f7de9c26fdaca9e6cdc16461d9
Anton Reshetov <reshetov.art@gmail.com>
2025-04-08 09:32:45 +0300

    feat(menu): add FormSection component for structured form layout

---
+++ src/renderer/components/ui/menu/FormSection.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+interface Props {
+  label: string
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+  <div class="[&:not(:first-child)]:mt-5">
+    <div class="pb-2 text-xs font-bold uppercase">
+      {{ label }}
+    </div>
+    <div class="space-y-5">
+      <slot />
+    </div>
+  </div>
+</template>

d9532edb73b9c35f3074c1dd40a00357ee25485d
Anton Reshetov <reshetov.art@gmail.com>
2025-04-08 11:43:44 +0300

    feat(editor): add prettier to format (#542)

--- src/main/ipc/index.ts
+++ src/main/ipc/index.ts
@@ -2,6 +2,7 @@ import type { Channel } from '../types/ipc'
 import { BrowserWindow } from 'electron'
 import { registerDBHandlers } from './handlers/db'
 import { registerDialogHandlers } from './handlers/dialog'
+import { registerPrettierHandlers } from './handlers/prettier'
 import { registerSystemHandlers } from './handlers/system'
 
 export function send(channel: Channel) {
@@ -12,4 +13,5 @@ export function registerIPC() {
   registerDialogHandlers()
   registerDBHandlers()
   registerSystemHandlers()
+  registerPrettierHandlers()
 }

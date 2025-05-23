756d373e2a6d6012041180e16aa0d2835e3694f2
Anton Reshetov <reshetov.art@gmail.com>
2025-05-23 15:38:01 +0300

    feat(ui): add Heading component for dynamic headings with optional description

---
+++ src/renderer/components/ui/heading/Heading.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+interface Props {
+  title: string
+  description?: string
+  level?: 2 | 3
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  level: 2,
+})
+
+const levelStyles = computed(() => {
+  return {
+    'text-lg font-bold': props.level === 2,
+    'text-md font-bold': props.level === 3,
+  }
+})
+</script>
+
+<template>
+  <div class="mb-3 not-first:mt-3">
+    <component
+      :is="`h${level}`"
+      :class="levelStyles"
+    >
+      {{ title }}
+    </component>
+    <p
+      v-if="description"
+      class="text-text-muted text-sm"
+    >
+      {{ description }}
+    </p>
+  </div>
+</template>

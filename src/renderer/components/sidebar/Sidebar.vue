c27a293960f49f08676682864c2eb108849d01aa
Anton Reshetov <reshetov.art@gmail.com>
2026-03-13 16:01:58 +0300

    feat(ui): add sidebar title "Code Snippets"

--- src/renderer/components/sidebar/Sidebar.vue
+++ src/renderer/components/sidebar/Sidebar.vue
@@ -7,12 +7,15 @@ import { i18n } from '@/electron'
     data-sidebar
     class="flex h-full flex-col px-1 pt-[var(--content-top-offset)]"
   >
+    <div class="truncate px-1 pb-2 font-bold select-none">
+      {{ i18n.t("sidebar.title") }}
+    </div>
     <UiText
       as="div"
       variant="caption"
       weight="bold"
       uppercase
-      class="flex gap-1 pt-2 pb-1 pl-1 select-none"
+      class="flex gap-1 pb-1 pl-1 select-none"
     >
       {{ i18n.t("sidebar.library") }}
     </UiText>

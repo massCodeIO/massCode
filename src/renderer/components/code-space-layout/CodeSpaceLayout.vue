6c0157be7df9d45c00c472ae57d94760c77bb9a4
Anton Reshetov <reshetov.art@gmail.com>
2026-03-27 17:03:30 +0300

    refactor: simplify CodeSpaceLayout and NotesSpace consumers
    Removed percentage-based twoPanel logic. Both now use pixel-based
    store values with LAYOUT_DEFAULTS fallbacks via ThreeColumn.

--- src/renderer/components/code-space-layout/CodeSpaceLayout.vue
+++ src/renderer/components/code-space-layout/CodeSpaceLayout.vue
@@ -1,85 +1,51 @@
 <script setup lang="ts">
-import * as Resizable from '@/components/ui/shadcn/resizable'
 import { useApp } from '@/composables'
 import { getCodePanels } from '@/composables/layoutModes'
 import { store } from '@/electron'
 
 const { codeLayoutMode } = useApp()
 
-const storedThreePanelLayout = store.app.get('code.layout.threePanel') as
-  | number[]
-  | undefined
-const storedTwoPanelLayout = store.app.get('code.layout.twoPanel') as
-  | number[]
-  | undefined
-const defaultThreePanelLayout = storedThreePanelLayout || [15, 20, 65]
-const defaultTwoPanelLayout = storedTwoPanelLayout || [35, 65]
 const panels = computed(() => getCodePanels(codeLayoutMode.value))
 
-function onLayout(layout: number[]) {
-  if (layout.length === 3) {
-    store.app.set('code.layout.threePanel', layout)
-    return
-  }
+const storedThreePanel = store.app.get('code.layout.threePanel') as
+  | number[]
+  | undefined
 
-  if (layout.length === 2) {
-    store.app.set('code.layout.twoPanel', layout)
-  }
+const sidebarWidth
+  = storedThreePanel?.length === 2 ? storedThreePanel[0] : undefined
+const listWidth = (() => {
+  if (storedThreePanel?.length === 2)
+    return storedThreePanel[1]
+  const twoPanel = store.app.get('code.layout.twoPanel') as number | undefined
+  return twoPanel ?? undefined
+})()
+
+function onResizeEnd(sw: number, lw: number) {
+  store.app.set('code.layout.threePanel', [sw, lw])
+}
+
+function onTwoPanelResize(lw: number) {
+  store.app.set('code.layout.twoPanel', lw)
 }
 </script>
 
 <template>
-  <div
-    v-if="!panels.showList"
-    class="h-screen"
+  <LayoutThreeColumn
+    :show-sidebar="panels.showSidebar"
+    :show-list="panels.showList"
+    :sidebar-width="sidebarWidth"
+    :list-width="listWidth"
+    @resize-end="onResizeEnd"
+    @two-panel-resize="onTwoPanelResize"
   >
-    <Editor />
-  </div>
-  <Resizable.ResizablePanelGroup
-    v-else-if="!panels.showSidebar"
-    direction="horizontal"
-    class="h-screen"
-    @layout="onLayout"
-  >
-    <Resizable.ResizablePanel
-      :default-size="defaultTwoPanelLayout[0]"
-      :min-size="15"
-    >
-      <SnippetList />
-    </Resizable.ResizablePanel>
-    <Resizable.ResizableHandle />
-    <Resizable.ResizablePanel
-      :default-size="defaultTwoPanelLayout[1]"
-      :min-size="30"
-    >
-      <Editor />
-    </Resizable.ResizablePanel>
-  </Resizable.ResizablePanelGroup>
-  <Resizable.ResizablePanelGroup
-    v-else
-    direction="horizontal"
-    class="h-screen"
-    @layout="onLayout"
-  >
-    <Resizable.ResizablePanel
-      :default-size="defaultThreePanelLayout[0]"
-      :min-size="10"
-    >
+    <template #sidebar>
       <Sidebar />
-    </Resizable.ResizablePanel>
-    <Resizable.ResizableHandle />
-    <Resizable.ResizablePanel
-      :default-size="defaultThreePanelLayout[1]"
-      :min-size="10"
-    >
+    </template>
+    <template #list>
       <SnippetList />
-    </Resizable.ResizablePanel>
-    <Resizable.ResizableHandle />
-    <Resizable.ResizablePanel
-      :default-size="defaultThreePanelLayout[2]"
-      :min-size="30"
-    >
+    </template>
+    <template #editor>
       <Editor />
-    </Resizable.ResizablePanel>
-  </Resizable.ResizablePanelGroup>
+    </template>
+  </LayoutThreeColumn>
 </template>
